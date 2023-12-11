c8177558931cab1a83fd70f123a0088dcd9a39aa
Paul K <pkerschieter@gmail.com>
2023-12-11 09:12:02 -0600

    finished up linking the skills db, creating routes and controllers

--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Require the controller that exports To-Do CRUD functions
+var skillsCtrl = require('../controllers/skills');
+
+// All actual paths begin with "/todos"
+
+// GET /todos
+router.get('/', skillsCtrl.index);
 
 module.exports = router;
