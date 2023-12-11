5489396940133bcd0d098cfb64d8fd3a396cff06
Paul K <pkerschieter@gmail.com>
2023-12-11 09:25:33 -0600

    lab part one done

--- routes/skills.js
+++ routes/skills.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-// Require the controller that exports To-Do CRUD functions
+// Require the controller that exports Skills CRUD functions
 var skillsCtrl = require('../controllers/skills');
 
-// All actual paths begin with "/todos"
+// All actual paths begin with "/skills"
 
-// GET /todos
+// GET /skills
 router.get('/', skillsCtrl.index);
+
+// GET /skills/:id
+router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
