4d4079902db8d2b7a6ec6146089f45c9f0bcf97c
Paul K <pkerschieter@gmail.com>
2023-12-11 20:14:51 -0600

    added the ability to delete skills

--- models/skill.js
+++ models/skill.js
@@ -7,8 +7,17 @@ const skills = [
 module.exports = {
     getAll,
     getOne,
-    create
+    create,
+    deleteOne
 };
+
+function deleteOne(id) {
+    // All properties attached to req.params are strings!
+    id = parseInt(id);
+    // Find the index based on the id of the todo object
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+}
 
 function create(skill) {
     // Add the id
--- routes/skills.js
+++ routes/skills.js
@@ -18,4 +18,6 @@ router.get('/:id', skillsCtrl.show);
 // POST /todos
 router.post('/', skillsCtrl.create);
 
+router.delete('/:id', skillsCtrl.delete);
+
 module.exports = router;
