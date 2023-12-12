fb7cf04fcde9af45bf76fbe3570b765ea59d20b0
Paul K <pkerschieter@gmail.com>
2023-12-11 18:49:06 -0600

    aded Add Skill Page

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,8 +3,13 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill
 };
+
+function newSkill(req, res) {
+    res.render('skills/new', { title: 'New Skill' });
+}
 
 function index(req, res) {
     res.render('skills/index', {
